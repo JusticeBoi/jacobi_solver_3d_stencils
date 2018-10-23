68a196f645ab21707405be832d800bc8667cde1f
Oztoprak <oguz.oztoprak@tum.de>
2018-10-23 16:34:11 +0200

    graphs show the average percent error in high order and non-uni
    - number of iteration vs. percent error graph for d1q3 and d1q5
    - todo: do the same thing you did in this commit to main_D3.py

--- main_D1_high_order.py
+++ main_D1_high_order.py
@@ -5,6 +5,7 @@ Created on Sep 7, 2018
 PROBLEM IS PRESSURE POISSON EQUATION IF P IS GIVEN AS P(X) = X² or P(X) = x³
 '''
 import numpy as np
+import matplotlib.pyplot as plt
 import kernel_high_order as ker
 from math import exp
 def change(ar):
@@ -48,15 +49,51 @@ b = np.array([2,2,2,2,2,2,2,-79])
 b_5 = np.array([24,24,24,24,24,24,105,-1272])
 b_trial = np.array([2,24,24,24,24,24,105,-79])
 
-relax = 2.0/3.0
-for i in range(200):
-     input_array_3=ker.ExecuteOneJacobiStepD1Q3(input_array_3,output_array_3, rhs_array_3, relax)
-     input_array_5=ker.ExecuteOneJacobiStepD1Q5(input_array_5,output_array_5, rhs_array_5, relax)
+real_sol =[(x+1)*(x+1) for x in range(12)]
+dif_3 = np.zeros(12)
+dif_5 = np.zeros(12)
 
-print("d1q3 : ",[x for x in output_array_3])
-print("d1q5 : ",[x for x in output_array_5])
+number_of_iter = 300 
+output_data_3_array = np.zeros(number_of_iter)
+output_data_5_array = np.zeros(number_of_iter)
+relax = 2.0/3.0
+percent_error_3_through_iterations = np.zeros(number_of_iter)
+percent_error_5_through_iterations = np.zeros(number_of_iter)
+for i in range(number_of_iter):
+    input_array_3=ker.ExecuteOneJacobiStepD1Q3(input_array_3,output_array_3, rhs_array_3, relax)
+    input_array_5=ker.ExecuteOneJacobiStepD1Q5(input_array_5,output_array_5, rhs_array_5, relax)
+    for j in range(12):
+        dif_3[j] = ((abs(output_array_3[j] - real_sol[j]))/real_sol[j]) * 100
+        dif_5[j] = ((abs(output_array_5[j] - real_sol[j]))/real_sol[j]) * 100
+    percent_error_3 = 0
+    percent_error_5 = 0
+    for j in range(12):
+        percent_error_3 += dif_3[j]
+        percent_error_5 += dif_5[j]
+    percent_error_3 /= 10
+    percent_error_5 /= 8
+    percent_error_3_through_iterations[i]= percent_error_3
+    percent_error_5_through_iterations[i]= percent_error_5
+
+
+
+
+#print("d1q3 : ",[x for x in output_array_3])
+#print("d1q5 : ",[x for x in output_array_5])
+
+a_range = np.arange(0.,number_of_iter)
+plt.plot(a_range,percent_error_3_through_iterations,'-r',label='D1Q3')
+plt.plot(a_range,percent_error_5_through_iterations,'-g',label='D1Q5')
+plt.ylabel('percent difference')
+plt.xlabel('iteration')
+plt.legend()
+plt.show()
+
+
+
+
 # print(np.linalg.solve(A,b))
 # b = np.linalg.solve(A,b)
-print(np.linalg.solve(A,b))
+#print(np.linalg.solve(A,b))
 #print(np.linalg.solve(A_5,b_5))
-print(np.linalg.solve(A_trial,b_trial))
+#print(np.linalg.solve(A_trial,b_trial))
